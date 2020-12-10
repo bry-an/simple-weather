6cf31fd952cb4fc7c4d636441bfcc29935385a97
Maksim Verkhoturov <sd32@protonmail.com>
2020-12-09 21:19:49 -0700

    adding Hour and Day components

---
+++ src/components/Day.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Day",
+  props: {
+    active: { type: Boolean, default: false },
+    dayOfTheWeek: { type: String, required: true },
+    skyCover: { type: String, required: true },
+    tempMax: { type: Number, required: true },
+    tempMin: { type: Number, required: true },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Hour.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Hour",
+  props: {
+    time: { type: Number, required: true },
+    temp: { type: Number, required: true },
+  },
+};
+</script>
+
+<style scoped>
+</style>
