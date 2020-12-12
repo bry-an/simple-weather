ed82bf464fff362d3047b2e6e10d8420d292f516
Maksim Verkhoturov <sd32@protonmail.com>
2020-12-11 21:29:56 -0700

    adding absolute imports for components

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@C': path.resolve(__dirname, 'src/components/')
+      }
+    }
+  }
+}
