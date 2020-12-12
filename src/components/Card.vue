3169d371f764cdf2530e75650f798986be0912d4
Maksim Verkhoturov <sd32@protonmail.com>
2020-12-11 21:28:36 -0700

    adjusting imports for components

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,17 @@
 <template>
-  <div></div>
+  <div><CardHeader /><CardFooter /></div>
 </template>
 
 <script>
+import CardHeader from '@C/CardHeader'
+import CardFooter from '@C/CardFooter'
+
 export default {
   name: 'Card',
+  components: {
+    CardHeader,
+    CardFooter
+  },
   props: {}
 }
 </script>
---
+++ src/components/CardFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'CardFooter',
+  components: {},
+  props: {}
+}
+</script>
+
+<style scoped>
+</style>
