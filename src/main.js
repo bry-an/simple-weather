f911a7b3999df10201a50fafd2db499ea56d0a73
Maksim Verkhoturov <sd32@protonmail.com>
2020-12-25 11:05:08 -0700

    subscribes for vuex store changes and dispatches getHourlyEight

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import { mapActions } from 'vuex'
 import App from './App.vue'
 import store from './store'
 
@@ -6,5 +7,22 @@ Vue.config.productionTip = false
 
 new Vue({
   store,
-  render: (h) => h(App)
+  render: (h) => h(App),
+  created() {
+    // const unsubscribe =
+    store.subscribe((mutation) => {
+      console.log(mutation)
+      // doesn't work because it runs before the API response comes back
+      // ok it actually does work but will it always work?
+      if (mutation.type === 'SET_SELECTED_DAY') {
+        this.$store.dispatch('getHourlyEight')
+      }
+    })
+
+    // you may call unsubscribe to stop the subscription
+    // unsubscribe()
+  },
+  methods: {
+    ...mapActions(['getHourlyEight'])
+  }
 }).$mount('#app')
