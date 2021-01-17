41008173a249780c9a5464b904cc02dd0624cac4
Bryan Yunis <bryany@booj.com>
2021-01-17 13:47:03 -0700

    Add base autocomplete and generic autocomplete components
    The base autocomplete can be used in any type of search. I extended its use for the location autocomplete

---
+++ src/components/Autocomplete/Autocomplete.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input v-model="localInput" type="text" @keydown.enter.prevent="changeSearchTerm" />
+    <p v-for="item in items" :key="item.name">
+      {{ item.name }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Autocomplete',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data: () => ({
+    localInput: ''
+  }),
+  methods: {
+    changeSearchTerm(e) {
+      this.$emit('change-search-term', e.target.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
